09a569cb74a600ad3bc169204ce5cd6ba594041d
cxcorp <joona.o.heikkila@gmail.com>
2017-06-28 10:20:48 +0300

    Keep bot alive on Heroku

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 const TgClient = require('node-telegram-bot-api')
 const fs = require('fs')
 const fetch = require('node-fetch')
+const express = require('express');
 require('dotenv').config()
 
 const TG_API_TOKEN = process.env.TG_API_TOKEN
-const INSPIRO_API_URL = process.env.INSPIRO_API_URL;
+const INSPIRO_API_URL = process.env.INSPIRO_API_URL
 
 /* don't have an SSL cert for webhook, use polling instead */
 const client = new TgClient(TG_API_TOKEN, { polling: true })
 client.onText(/\/inspireme/, inspireCommand)
+
+if (process.env.HEROKU) {
+    // heroku kills web dynos after 60s if they don't bind to PORT
+    redirectIndexToBot()
+}
 
 function inspireCommand(msg) {
     const headers = { 'User-Agent': getUserAgent() }
@@ -31,4 +37,12 @@ function inspireCommand(msg) {
 
 function getUserAgent() {
     return 'Inspiro Telegram Bot v0.2.1 (github.com/cxcorp/inspirobot_bot)'
+}
+
+function redirectIndexToBot() {
+    const convoUrl = process.env.BOT_CONVO_URL || 'https://telegram.me/inspirobot_bot'
+    const port = parseInt(process.env.PORT || '1234', 10)
+    const app = express()
+    app.get('/', (req, res) => res.redirect(convoUrl))
+    app.listen(port)
 }
